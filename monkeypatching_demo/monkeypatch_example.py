739ed961b96e81aeecfb5777d86d79f9826b4f8d
David Kremer <david.kremer@sewan.fr>
2017-01-15 20:10:27 +0100

    update monkeypatch_example

--- monkeypatching_demo/monkeypatch_example.py
+++ monkeypatching_demo/monkeypatch_example.py
@@ -29,27 +29,28 @@ def test_monkeypatch(monkeypatch):
     """ Simple test function to demonstrate the monkeypatching process """
 
     # Instanciate the objects we are going to use
-    tested_object = A()
+    clear_object = A()
 
     # Use class methods before monkeypatching
     print("Before monkeypatching")
-    print(tested_object.A1(),tested_object.A2())
+    clear_object.A1()
+    clear_object.A2()
 
 
     # what is going on after monkeypatching ?
     print("After monkeypatching")
     mock_object = MagicMock( return_value = "You just have been monkeypatched. Please keep going" )
     monkeypatch.setattr( "to_monkeypatch.B.B1", mock_object )
+    patched_object = A()
+    patched_object.A1()  # must call to_monkeypatch.modB.B1 (which is actually monkeypatched)
+    patched_object.A2()  # must call to_monkeypatch.modB.B2 (which the initial commited method)
 
-    tested_object.A1()  # must call to_monkeypatch.modB.B1 (which is actually monkeypatched)
-    tested_object.A2()  # must call to_monkeypatch.modB.B2 (which the initial commited method)
-
-    assert mock_object.called is True
+    assert mock_object.called
     assert mock_object.call_count == 1
 
     n_repeat = 10
     for x in range(n_repeat):
-        my = tested_object.A1()
+        my = patched_object.A1()
 
     assert mock_object.call_count == 1 + n_repeat
 
