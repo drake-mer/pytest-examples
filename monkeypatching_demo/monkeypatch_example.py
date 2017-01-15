e14c06e1d1e3a0de7fc16e9598adabd23e5b5b8c
David Kremer <david.kremer@sewan.fr>
2017-01-15 20:11:46 +0100

    rename to_monkeypatch into module_to_monkeypatch

--- monkeypatching_demo/monkeypatch_example.py
+++ monkeypatching_demo/monkeypatch_example.py
@@ -7,13 +7,13 @@ except ImportError:
     from mock import MagicMock
 
 # the things we are going to monkeypatch
-import to_monkeypatch
+import module_to_monkeypatch
 
 
 
 class A():
     def __init__(self):
-        self.b_obj = to_monkeypatch.B()
+        self.b_obj = module_to_monkeypatch.B()
 
     def A1(self):
         return "Our A() type object, calling B.B1:"
@@ -40,10 +40,10 @@ def test_monkeypatch(monkeypatch):
     # what is going on after monkeypatching ?
     print("After monkeypatching")
     mock_object = MagicMock( return_value = "You just have been monkeypatched. Please keep going" )
-    monkeypatch.setattr( "to_monkeypatch.B.B1", mock_object )
+    monkeypatch.setattr( "module_to_monkeypatch.B.B1", mock_object )
     patched_object = A()
-    patched_object.A1()  # must call to_monkeypatch.modB.B1 (which is actually monkeypatched)
-    patched_object.A2()  # must call to_monkeypatch.modB.B2 (which the initial commited method)
+    patched_object.A1()  # must call module_to_monkeypatch.modB.B1 (which is actually monkeypatched)
+    patched_object.A2()  # must call module_to_monkeypatch.modB.B2 (which the initial commited method)
 
     assert mock_object.called
     assert mock_object.call_count == 1
